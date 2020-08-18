499f04d4764a9b460ec53393a0759d7f0120e630
Olivia <ojack@Olivias-MBP.fritz.box>
2020-08-18 20:41:26 +0200

    no longer store names on server, possible fix for server rooms

--- server.js
+++ server.js
@@ -34,7 +34,6 @@ if (process.env.TWILIO_SID) {
   var io = require('socket.io')(httpsServer);
 
 
-  //crear un servidor en puerto 8000
   httpsServer.listen(portNumber, () => {
     //print the ip address on the console
     //console.log("server available in https://"+myip.getLocalIP4()+":8000")
@@ -42,46 +41,13 @@ if (process.env.TWILIO_SID) {
   });
 
 
-
-  //data structures for storing persistent user ids.
-  //connected clients should use uuid (rather than socket id) for identifying a user
-
-  //look up uuid by entiring socket id
-  var userFromSocket = {}
-
-  //lookup socket id by entering uuid
-  var socketFromUser = {}
-
-
   //new connection to websocket server
   io.on('connection', function (socket) {
-    console.log("new connection", socket.id)
-
-    var thisRoom = null
-
     socket.on('join', function (room, _userData) {
-      thisRoom = room
-
-      console.log("user", JSON.stringify(_userData))
-      if (_userData.uuid) {
-        userFromSocket[socket.id] = _userData.uuid
-        socketFromUser[_userData.uuid] = socket.id
-      } else {
-        console.log("no user data!")
-      }
       // Get the list of peers in the room
       var peers = io.nsps['/'].adapter.rooms[room] ?
         Object.keys(io.nsps['/'].adapter.rooms[room].sockets) : []
 
-      io.of('/').in(room).clients(function (error, clients) {
-        if (error) throw error;
-        console.log("clients error!")
-        console.log(clients); // => [Anw2LatarvGVVXEIAAAD]
-      });
-
-      var peerUuids = peers.map(function (socketId) {
-        return userFromSocket[socketId]
-      })
       // only use twilio if available
       //------twilio confusions
       //console.log(client)
@@ -92,7 +58,7 @@ if (process.env.TWILIO_SID) {
             // console.log(client)
             // console.log(token.iceServers)
             // socket.emit('servers', socket.id, token.iceServers)
-            socket.emit('ready', socket.id, peerUuids, token.iceServers)
+            socket.emit('ready', socket.id, peers, token.iceServers)
 
 
             // And then add the client to the room
@@ -100,16 +66,16 @@ if (process.env.TWILIO_SID) {
 
             //send updated list of peers to all clients in room
             // io.sockets.emit('peers', peerUuids);
-            socket.to(thisRoom).emit('new peer', _userData.uuid)
+            socket.to(room).emit('new peer', socket.id)
           })
       } else {
-        socket.emit('ready', socket.id, peerUuids)
+        socket.emit('ready', socket.id, peers)
         // And then add the client to the room
         socket.join(room);
 
         //send updated list of peers to all clients in room
         // io.sockets.emit('peers', peerUuids);
-        socket.to(thisRoom).emit('new peer', _userData.uuid)
+        socket.to(room).emit('new peer', socket.id)
       }
       // -----------twilio confusions ends
 
@@ -119,30 +85,26 @@ if (process.env.TWILIO_SID) {
 
     });
     // client can request updated list of peers
-    socket.on("getPeers", function (data) {
-      var peers = io.nsps["/"].adapter.rooms[thisRoom] ?
-        Object.keys(io.nsps["/"].adapter.rooms[thisRoom].sockets) : [];
+    socket.on("getPeers", function (room, data) {
+      var peers = io.nsps["/"].adapter.rooms[room] ?
+        Object.keys(io.nsps["/"].adapter.rooms[room].sockets) : [];
 
-      var peerUuids = peers.map(function (socketId) {
-        return userFromSocket[socketId];
-      });
-
-      socket.emit("peers", peerUuids);
+      socket.emit("peers", peers);
     });
 
-    socket.on('broadcast', function (data) {
+    socket.on('broadcast', function (room, data) {
       // io.sockets.emit('broadcast', data)
-      console.log("broadcasting", data, socket.room)
+    //  console.log("broadcasting", data, socket.room)
       //  io.sockets.in(socket.room).emit('broadcast', data)
-      socket.to(thisRoom).emit('broadcast', data)
+      socket.to(room).emit('broadcast', data)
 
     });
 
     socket.on('signal', function (data) {
       //console.log("forwarding signal " + JSON.stringify(data))
-      var client = io.sockets.connected[socketFromUser[data.id]];
+      var client = io.sockets.connected[data.id];
       client && client.emit('signal', {
-        id: userFromSocket[socket.id],
+        id: socket.id,
         label: socket.label,
         signal: data.signal,
       });
